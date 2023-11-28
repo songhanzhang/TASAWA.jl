e5ab0dd2790fc06a49f3f8863c306b3a0987e37a
Songhan ZHANG <shz@dlut.edu.cn>
2023-11-28 20:42:10 +0800

    Create div_k_types.jl

---
+++ src/div_k_types.jl
@@ -0,0 +1,22 @@
+function div_k_types(k_save)
+
+    k_real = zeros(size(k_save))*NaN
+    k_imag = zeros(size(k_save))*NaN
+    k_cplx = zeros(size(k_save))*(NaN+NaN*1im)
+    for ii = 1:size(k_save,1)
+        for jj = 1:size(k_save,2)
+            k = k_save[ii,jj]
+            if abs(real(k))>1e-3 && abs(imag(k))<1e-3
+                k_real[ii,jj] = real(k_save[ii,jj])
+                cg_real[ii,jj] = real(cg_save[ii,jj])
+            elseif abs(real(k))<1e-3 && abs(imag(k))>1e-3
+                k_imag[ii,jj] = imag(k_save[ii,jj])
+            elseif abs(real(k))>1e-3 && abs(imag(k))>1e-3
+                k_cplx[ii,jj] = k_save[ii,jj]
+            end
+        end
+    end
+
+    return k_real, k_imag, k_cplx
+
+end
