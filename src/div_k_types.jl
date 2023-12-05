ca6ec7c3284f34d6eb2ef0432504e383c6991b8a
Songhan ZHANG <shz@dlut.edu.cn>
2023-12-05 19:39:54 +0800

    Update div_k_types.jl

--- src/div_k_types.jl
+++ src/div_k_types.jl
@@ -1,4 +1,4 @@
-function div_k_types(k_save)
+function div_k_types(k_save, tol)
 
     k_real = zeros(size(k_save))*NaN
     k_imag = zeros(size(k_save))*NaN
@@ -6,12 +6,12 @@ function div_k_types(k_save)
     for ii = 1:size(k_save,1)
         for jj = 1:size(k_save,2)
             k = k_save[ii,jj]
-            if abs(real(k))>1e-3 && abs(imag(k))<1e-3
+            if abs(real(k))>1e-3 && abs(imag(k))<tol
                 k_real[ii,jj] = real(k_save[ii,jj])
-                cg_real[ii,jj] = real(cg_save[ii,jj])
-            elseif abs(real(k))<1e-3 && abs(imag(k))>1e-3
+                # cg_real[ii,jj] = real(cg_save[ii,jj])
+            elseif abs(real(k))<1e-3 && abs(imag(k))>tol
                 k_imag[ii,jj] = imag(k_save[ii,jj])
-            elseif abs(real(k))>1e-3 && abs(imag(k))>1e-3
+            elseif abs(real(k))>1e-3 && abs(imag(k))>tol
                 k_cplx[ii,jj] = k_save[ii,jj]
             end
         end
