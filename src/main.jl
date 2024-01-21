040ea10a0949f7d788400ebed802892d62c14703
Songhan ZHANG <shz@dlut.edu.cn>
2024-01-21 23:21:15 +0800

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -78,20 +78,7 @@ for i_DOF = 1:floor(Int,n_DOF/3)
 end
 K2g_hat = real(1im*T'*K2g*T)
 f_ax = (0.00001:0.00001:0.06)*1e6
-k_save = zeros(2*n_DOF,length(f_ax))*(0+0im)
-Ψ_save = zeros(2*n_DOF, 2*n_DOF, length(f_ax))*(0+0im)
-
-for (i_freq,freq) in enumerate(f_ax)
-    @printf "Current progress: %.2f%%\n" i_freq/length(f_ax)*100
-    ω = 2*pi*freq
-    A_eig = [zeros(n_DOF,n_DOF)  K3g-ω^2*Mg
-             K3g-ω^2*Mg        K2g_hat]
-    B_eig = [K3g-ω^2*Mg        zeros(n_DOF,n_DOF)
-             zeros(n_DOF,n_DOF)  -K1g]
-    (Λ,Φ) = eigen(A_eig,B_eig)
-    k_save[:,i_freq] = Λ
-    Ψ_save[:,:,i_freq] = Φ
-end
+(k_save,Ψ_save) = sol_eigen(K1g,K2g_hat,K3g,f_ax)
 (k_real, k_imag, k_cplx) = div_k_types(k_save, tol = 1e-3)
 
 fig_kw = plot(
